cfa6779b3b2ec968f420c175dbbaf9c00559ee16
Jihye19249205 <119666066+Jihye19249205@users.noreply.github.com>
2023-01-17 22:14:02 +0000

    [update]4-27

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,32 @@
 class BooksController < ApplicationController
-  def index
+  
+  def create
+    @book = Book.new(books_params)
+    @book.user_id = current_user.id
+    if @book.save
+    redirect_to books_path
+    # else
+    #   render :index
+    end
   end
-
+  
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+  
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    # @book = Book.find(params[:id])
   end
+
+  private
+
+  def books_params
+    params.require(:book).permit(:title, :body)
+  end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+  
+  # before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
+     @user = User.new
+     @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   def edit
+    @user = User.find(params[:id])
   end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+    @book = current_user.book.update
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:profile_image, :name, :introduction)
+  end
+  
 end
